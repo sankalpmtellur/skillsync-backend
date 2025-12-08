698419763c3f3bc41cb3024262451285c6645626
sankalpmtellur <sankalpmtellur282006@gmail.com>
2025-12-08 14:26:34 +0530

    Add initial Prisma migration for PostgreSQL

---
+++ prisma/migrations/20251208085618_initial/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "skills" TEXT,
+    "experience" TEXT,
+    "location" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "teamSize" INTEGER NOT NULL,
+    "duration" TEXT NOT NULL,
+    "difficulty" TEXT NOT NULL,
+    "progress" INTEGER,
+    "views" INTEGER NOT NULL DEFAULT 0,
+    "tags" JSONB,
+    "featured" BOOLEAN NOT NULL DEFAULT false,
+    "ownerId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
